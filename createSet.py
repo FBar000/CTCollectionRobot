437addc4e0e8cb62f9257203e86a57a076ace682
FBar000 <barrera.federico@outlook.com>
2022-08-01 22:24:20 -0400

    Update

--- createSet.py
+++ createSet.py
@@ -5,6 +5,37 @@ Program to automatically create a set of items.
 """
 from methods import *
 import json
+import time
+from selenium.webdriver.common.by import By
+
+def createSet(driver, object_identifiers):
+    """
+    Create a set of objects for manual batch editing.
+
+    Uses batch editing in Collective Access.
+
+    Parameters;
+        driver : Selenium webDriver element
+        object_identifiers (list[string]) : A list of the object identifiers for the objects to be accessioned
+    Return:
+        None
+    """
+    driver.implicitly_wait(5)
+    if not driver.current_url.endswith('/index.php/manage/sets/SetEditor/Edit/set_id/0/type_id/12/table_num/57'):            # Fix this; Not working
+        driver.get("/".join(driver.current_url.split('/')[:3])+'/index.php/manage/sets/SetEditor/Edit/set_id/0/type_id/12/table_num/57')
+    box = driver.find_element(By.XPATH, "//div[contains(text(), 'Add')]/input")
+    for ident in object_identifiers:
+        try:
+            box.send_keys(ident)
+            driver.find_element(By.XPATH, f"//a[contains(text(), '{ident}')]").click()
+            time.sleep(0.5)
+        except:
+            box.clear()
+            pass
+    # Save
+    time.sleep(0.75)
+    driver.find_element(By.XPATH, "//a/*[contains(text(), 'Save')]").click()
+
 
 if __name__ == '__main__':
 
@@ -14,7 +45,10 @@ if __name__ == '__main__':
     with open(credential_file, 'r') as jsonFile:
         credential = json.load(jsonFile)
 
+    # Create set of objects
+    objects = makeHandles()    
+
     # Navigate to sets
     dr = session(credential['username'], credential['password'], credential['url'])
-    bulkLocationSet(dr, ['A2022.DowdCharles.017'], 'Storage locations / Lee Academy / Lateral File 1 / Draw 3')
+    createSet(dr, objects)
     dr.quit()
